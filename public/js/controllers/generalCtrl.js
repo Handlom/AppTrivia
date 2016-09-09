632e6ac37dc1fa49ff88e1ead4fbc7514cfc17c9
RogerCG <roger.canchanya.syp@handlom.com>
2016-09-09 16:55:57 -0500

    Cuarto punto/checkpoint - Implementación de audio basico

--- public/js/controllers/generalCtrl.js
+++ public/js/controllers/generalCtrl.js
@@ -1,16 +1,28 @@
 'use strict';
 app.controller("generalCtrl", function($scope, $location){
 	$scope.saludo="Hola";  
-
+	$scope.audios="audio/stand.mp3";
 	$scope.classAnimate='animated swing';
-	
+
 	$(document).ready(function(){
 	    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered	    
 	    $('.modal-trigger').leanModal();
 	    $('.carousel.carousel-slider').carousel({full_width: true});
 
 	});
-
+	$scope.enviarp1= function(altPregunta1){
+		$scope.audios='';
+		if (altPregunta1='opcion2') {
+			$scope.audios="audio/correct.mp3";
+			console.log($scope.audios);
+		}
+		else{
+			$scope.audios="audio/incorrect.mp3";
+			console.log($scope.audios);
+		}
+		console.log(altPregunta1);
+	}
+	
 
 
 });
