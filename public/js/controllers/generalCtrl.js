646da82bd24bf0c85ffa296836bc8f7656264843
RogerCG <roger.canchanya.syp@handlom.com>
2016-09-09 15:53:43 -0500

    Tercer punto/checkpoint - Uso del framework materializecss y animaciones basicas

--- public/js/controllers/generalCtrl.js
+++ public/js/controllers/generalCtrl.js
@@ -1,4 +1,16 @@
 'use strict';
 app.controller("generalCtrl", function($scope, $location){
-	$scope.saludo="Hola";     
+	$scope.saludo="Hola";  
+
+	$scope.classAnimate='animated swing';
+	
+	$(document).ready(function(){
+	    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered	    
+	    $('.modal-trigger').leanModal();
+	    $('.carousel.carousel-slider').carousel({full_width: true});
+
+	});
+
+
+
 });
