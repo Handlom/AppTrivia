62268534400b34bdae9ae0f7d17efdf6d6c4bcf2
RogerCG <roger.canchanya.syp@handlom.com>
2016-09-09 11:15:29 -0500

    Segundo punto/checkpoint - App estructurada bajo patron SPA con angularJS y Materialize

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+'use strict'//Define que el código JavaScript deba ejecutarse en el "modo estricto".
+
+var app = angular.module("appTrivia", [
+	'ngResource',
+	'ngRoute',
+	'firebase'
+])
+.config(function ($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'templates/bienvenido.html',
+			controller:'generalCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+/*.run(function($rootScope, $location, sessionControl){
+
+	function inhabilitar(){return false;}
+	document.oncontextmenu=inhabilitar;
+
+	var rutasPrivadas = ['/bienvenido'];
+	$rootScope.$on('$routeChangeStart', function(){
+		//Si en el path es igual a una ruta privada y si mi logueo es falso entro al if
+		//Si mi path es diferente al una ruta privada y mi logueo es true no entro al if 
+		//Si el path y las rutasPrivadas no son iguales devolvera -1		
+		if (($.inArray($location.path(), rutasPrivadas) !== -1) &&  !sessionControl.isLoggedIn()) {
+			//console.log('dentro del 1er if: '+sessionControl.statusUser());
+			console.error('Debe iniciar sesión para poder continuar');
+			$location.path('/');
+		}
+	});
+});*/
---
+++ public/js/controllers/generalCtrl.js
@@ -0,0 +1,4 @@
+'use strict';
+app.controller("generalCtrl", function($scope, $location){
+	$scope.saludo="Hola";     
+});
